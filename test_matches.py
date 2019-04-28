879b268da3a0a460623fa5d324c035919481d839
Jonas Kaerts <jonaskaerts@gmail.com>
2019-04-28 20:05:58 +0200

    added tests for the stack property

--- test_matches.py
+++ test_matches.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import deque
 from Matches import Match, DelimiterStack
 
 class TestDelimiterStack(unittest.TestCase):
@@ -8,6 +9,11 @@ class TestDelimiterStack(unittest.TestCase):
         test_stack = DelimiterStack()
         self.assertTrue(test_stack.is_empty())
         
+    def test_stack_property(self):
+        test_stack = DelimiterStack([('(', 1),('[', 2),(']', 2)])
+        self.assertEqual(test_stack.stack, deque([('(', 1),('[', 2),(']', 2)]))
+        with self.assertRaises(AttributeError):
+            test_stack.stack = deque([('(', 1)])
         
 if __name__ == "__main__":
     unittest.main()
