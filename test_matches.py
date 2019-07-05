b9f42a7a59012aa23afdbdedaf326683dcb743b9
Jonas Kaerts <jonaskaerts@gmail.com>
2019-07-05 13:25:59 +0200

    Changed tests to reflect name changes in data structures

--- test_matches.py
+++ test_matches.py
@@ -1,20 +1,20 @@
 import unittest
 from collections import deque
-from Matches import DelimiterStack
+from Matches import DelimiterDeque
 
 
-class TestDelimiterStack(unittest.TestCase):
+class TestDelimiterDeque(unittest.TestCase):
     def test_is_empty(self):
-        test_stack = DelimiterStack([('(', 1), ('[', 2), (']', 2)])
-        self.assertFalse(test_stack.is_empty())
-        test_stack = DelimiterStack()
-        self.assertTrue(test_stack.is_empty())
+        test_deque = DelimiterDeque([('(', 1), ('[', 2), (']', 2)])
+        self.assertFalse(test_deque.is_empty())
+        test_deque = DelimiterDeque()
+        self.assertTrue(test_deque.is_empty())
 
-    def test_stack_property(self):
-        test_stack = DelimiterStack([('(', 1), ('[', 2), (']', 2)])
-        self.assertEqual(test_stack.stack, deque([('(', 1), ('[', 2), (']', 2)]))
+    def test_deque_property(self):
+        test_deque = DelimiterDeque([('(', 1), ('[', 2), (']', 2)])
+        self.assertEqual(test_deque.deque, deque([('(', 1), ('[', 2), (']', 2)]))
         with self.assertRaises(AttributeError):
-            test_stack.stack = deque([('(', 1)])
+            test_deque.deque = deque([('(', 1)])
 
 
 if __name__ == "__main__":
