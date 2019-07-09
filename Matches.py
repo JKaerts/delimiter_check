fe99d896346f6fab3bc5d3d89f184457af4bbf16
Jonas Kaerts <jonaskaerts@gmail.com>
2019-07-09 15:51:50 +0200

    Added doctests to Matches

--- Matches.py
+++ Matches.py
@@ -1,7 +1,10 @@
 from collections import deque
 from typing import Tuple, Deque
 
-
+''' A Match is a type variable standing for tuple of a string and an integer.
+    The string stands for the delimiter found and the integer stands for
+    the line on which the delimiter was found.
+'''
 Match = Tuple[str, int]
 
 
@@ -19,18 +22,61 @@ class MatchDeque:
         return self._deque
 
     def appendleft(self, new_match: Match):
+        ''' Appends a new element to the left of the deque.
+
+            >>> matches = MatchDeque()
+            >>> matches.appendleft(('{', 1))
+            >>> matches.appendleft(('[', 5))
+            >>> matches.deque
+            deque([('[', 5), ('{', 1)])
+        '''
         self.deque.appendleft(new_match)
 
     def appendright(self, new_match: Match):
+        ''' Appends a new element to the right of the deque.
+
+            >>> matches = MatchDeque()
+            >>> matches.appendright(('{', 1))
+            >>> matches.appendright(('[', 5))
+            >>> matches.deque
+            deque([('{', 1), ('[', 5)])
+        '''
         self.deque.append(new_match)
 
     def popleft(self):
+        ''' Removes and returns the leftmost element of the deque.
+
+            >>> matches = MatchDeque([('{', 1), ('[', 5)])
+            >>> result = matches.popleft()
+            >>> result
+            ('{', 1)
+            >>> matches.deque
+            deque([('[', 5)])
+        '''
         return self.deque.popleft()
 
     def popright(self):
+        ''' Removes and returns the rightmost element of the deque.
+
+            >>> matches = MatchDeque([('{', 1), ('[', 5)])
+            >>> result = matches.popright()
+            >>> result
+            ('[', 5)
+            >>> matches.deque
+            deque([('{', 1)])
+        '''
         return self.deque.pop()
 
     def is_empty(self):
+        ''' Checks if the deque is empty.
+
+            >>> matches1 = MatchDeque([('(', 1), ('[', 2), (']', 2)])
+            >>> matches1.is_empty()
+            False
+            >>> matches2 = MatchDeque()
+            >>> matches2.is_empty()
+            True
+        '''
         return not bool(self.deque)
 
     def __iter__(self):
@@ -38,3 +84,8 @@ class MatchDeque:
 
     def __next__(self):
         return next(self.deque)
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
