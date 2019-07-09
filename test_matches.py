8df11b59dc5ba3c7ea5e96acd5b8cf8cb492c9a0
Jonas Kaerts <jonaskaerts@gmail.com>
2019-07-09 15:52:58 +0200

    Added test case for appending

--- test_matches.py
+++ test_matches.py
@@ -4,6 +4,13 @@ from Matches import MatchDeque
 
 
 class TestMatchDeque(unittest.TestCase):
+
+    def test_appendleft(self):
+        test_deque = MatchDeque([('(', 1), ('[', 2), (']', 2)])
+        test_deque.appendleft(('{', 4))
+        self.assertEqual(test_deque.deque,
+                         deque([('{', 4), ('(', 1), ('[', 2), (']', 2)]))
+
     def test_is_empty(self):
         test_deque = MatchDeque([('(', 1), ('[', 2), (']', 2)])
         self.assertFalse(test_deque.is_empty())
@@ -12,7 +19,8 @@ class TestMatchDeque(unittest.TestCase):
 
     def test_deque_property(self):
         test_deque = MatchDeque([('(', 1), ('[', 2), (']', 2)])
-        self.assertEqual(test_deque.deque, deque([('(', 1), ('[', 2), (']', 2)]))
+        self.assertEqual(test_deque.deque,
+                         deque([('(', 1), ('[', 2), (']', 2)]))
         with self.assertRaises(AttributeError):
             test_deque.deque = deque([('(', 1)])
 
