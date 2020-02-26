b832c4523e8c0fe69efd67e02db3dba78b7b4291
Jonas Kaerts <jonaskaerts@gmail.com>
2020-02-26 13:32:44 +0100

    Inlined argument parsing in the script

--- delimiter_check.py
+++ delimiter_check.py
@@ -17,6 +17,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import argparse
 import sys
 from argument_parser import parse_args
 from Matches import MatchDeque
