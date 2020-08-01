5942517b2d239b3324cdf9533c53aa25262bea88
Jonas Kaerts <jonas@jkaerts.net>
2020-08-01 14:11:08 +0200

    explicitly state function signature for clarity

--- test/test_delimiter_check.py
+++ test/test_delimiter_check.py
@@ -3,26 +3,26 @@ import unittest
 
 class TestMatching(unittest.TestCase):
     def test_parentheses_match(self):
-        self.assertTrue(delimiters_match('(', ')'))
+        self.assertTrue(delimiters_match(left='(', right=')'))
     
     def test_braces_match(self):
-        self.assertTrue(delimiters_match('{', '}'))
+        self.assertTrue(delimiters_match(left='{', right='}'))
     
     def test_brackets_match(self):
-        self.assertTrue(delimiters_match('[', ']'))
+        self.assertTrue(delimiters_match(left='[', right=']'))
     
     def test_order_matters(self):
-        self.assertFalse(delimiters_match(')', '('))
-        self.assertFalse(delimiters_match('}', '{'))
-        self.assertFalse(delimiters_match(']', '['))
+        self.assertFalse(delimiters_match(left=')', right='('))
+        self.assertFalse(delimiters_match(left='}', right='{'))
+        self.assertFalse(delimiters_match(left=']', right='['))
 
 class TestLineParsing(unittest.TestCase):
     def test_empty_line_returns_empty_list(self):
-        self.assertEqual([], get_matches_from_line(0, ""))
+        self.assertEqual([], get_matches_from_line(number=0, line=""))
     
     def test_all_matches_are_delimiters(self):
         line_text = "A (short) text [with {many} delimiters]"
-        found_matches = get_matches_from_line(0, line_text)
+        found_matches = get_matches_from_line(number=0, line=line_text)
         found_delimiters, _ = zip(*found_matches)
         
         self.assertTrue(all([delimiter in ALL_DELIMITERS for delimiter in found_delimiters]))
@@ -30,14 +30,14 @@ class TestLineParsing(unittest.TestCase):
     def test_finds_delimiters_in_order(self):
         line_text = "A (short) text [with {many} delimiters]"
         expected_delimiters = ['(', ')', '[', '{', '}', ']']
-        found_matches = get_matches_from_line(0, line_text)
+        found_matches = get_matches_from_line(number=0, line=line_text)
         found_delimiters, _ = zip(*found_matches)
         
         self.assertEqual(expected_delimiters, list(found_delimiters))
         
     def test_found_matches_contains_line_number(self):
         line_text = "A (short) text [with {many} delimiters]"
-        found_matches = get_matches_from_line(4, line_text)
+        found_matches = get_matches_from_line(number=4, line=line_text)
         _, found_linenumbers = zip(*found_matches)
         
         self.assertTrue(all([number == 4 for number in found_linenumbers]))
