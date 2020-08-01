768e2ef8a9cea25daf0e10e9f1afd018efa826d3
Jonas Kaerts <jonas@jkaerts.net>
2020-08-01 17:34:59 +0200

    Make extra tests

--- delimiter_check/__init__.py
+++ delimiter_check/__init__.py
@@ -1,3 +1,4 @@
 from .delimiter_check import delimiters_match, \
     get_matches_from_line, \
+    matches_top_of_stack, \
     ALL_DELIMITERS
--- test/test_delimiter_check.py
+++ test/test_delimiter_check.py
@@ -45,5 +45,22 @@ class TestLineParsing(unittest.TestCase):
         self.assertTrue(all([number == 4 for number in found_linenumbers]))
 
 
+class TestStackMatching(unittest.TestCase):
+    def test_empty_stack_returns_false(self):
+        self.assertFalse(matches_top_of_stack('(', []))
+
+    def test_matches_correct_delimiter(self):
+        # A ']' has been found on line 1 and a '(' has been found on line 4
+        current_matches = [(']', 1), ('(', 4)]
+        new_delimiter = ')'
+        self.assertTrue(matches_top_of_stack(delimiter=new_delimiter, stack=current_matches))
+
+    def test_delimiters_must_have_right_order(self):
+        # A '(' has been found on line 1 and a ']' has been found on line 4
+        current_matches = [('(', 1), (']', 4)]
+        new_delimiter = '['
+        self.assertFalse(matches_top_of_stack(delimiter=new_delimiter, stack=current_matches))
+
+
 if __name__ == '__main__':
     unittest.main()
