39697f5d798f56de31da46e95e139b0ad427a773
Jonas Kaerts <jonaskaerts@gmail.com>
2021-08-15 11:26:12 +0200

    Replace Makefile with custom build script.

---
+++ build.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+
+
+def show_help():
+    print('''Build targets
+  help        - Show this message
+  test        - Run the test suite
+  typecheck   - Run mypy to check the typing of the project
+  stylecheck  - Use pycodestyle to check pep8-compliance
+''')
+
+
+if len(sys.argv) == 1:
+    show_help()
+    sys.exit()
+
+arg = sys.argv[1]
+
+if arg == 'help':
+    show_help()
+elif arg == 'test':
+    subprocess.run(['python', 'test_delimiter_check.py'])
+elif arg == 'typecheck':
+    subprocess.run(['mypy', 'delimiter_check.py'])
+elif arg == 'stylecheck':
+    subprocess.run(['pycodestyle', '.'])
