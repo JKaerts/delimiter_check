80fbe5c5067fd7234bdfd1d26eb1513a2ba08ff3
Jonas Kaerts <jonaskaerts@gmail.com>
2021-08-15 11:29:10 +0200

    Style edit on stress test

--- stresstest.py
+++ stresstest.py
@@ -3,11 +3,13 @@ from time import time
 
 from delimiter_check import get_results_from_file
 
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-    "u", "v", "w", "x", "y", "z", "0", "1", "2", "3",
-    "4", "5", "6", "7", "8", "9", "(", ")", "[", "]",
-    "{", "}"]
+alphabet = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+        "u", "v", "w", "x", "y", "z", "0", "1", "2", "3",
+        "4", "5", "6", "7", "8", "9", "(", ")", "[", "]",
+        "{", "}"
+]
 
 print("Creating file for the stress test.")
 with open('testing_file', 'w') as outfile:
@@ -27,4 +29,3 @@ end = time()
 print(f'''Stress test complete. It took
     {end - start}
 seconds. You may now close the temporary testing file.''')
-
